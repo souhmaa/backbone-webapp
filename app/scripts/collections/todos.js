50eec4b99d829fe901b7daa92f58d33f524f00ec
souheil <maalaoui.ENSI@gmail.com>
2017-01-30 23:54:04 +0100

    Add mvc todo app source files

---
+++ app/scripts/collections/todos.js
@@ -0,0 +1,29 @@
+var app = app || {};
+
+(function() {
+  'use strict';
+
+  var Todos = Backbone.Collection.extend({
+    model: app.Todo,
+
+    localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+    completed: function() {
+      return this.where({completed: true});
+    },
+
+    remaining: function() {
+      return this.where({completed: false});
+    },
+
+    nextOrder: function() {
+      return this.length ? this.last().get('order') + 1 : 1;
+    },
+
+    comparator: 'order'
+
+  });
+
+  app.todos = new Todos();
+
+})();
